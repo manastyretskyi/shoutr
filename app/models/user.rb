b5a0a683fb6b8485e6a245bd9cd895698a2a664d
Liubomyr <lyubomur.user.mv@gmail.com>
2020-03-28 19:28:06 +0200

    Fix sign up and null: false to shouts and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   include Clearance::User
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
   has_many :shouts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_121944) do
+ActiveRecord::Schema.define(version: 2020_03_28_172553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shouts", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "content_type"
-    t.integer "content_id"
+    t.string "content_type", null: false
+    t.integer "content_id", null: false
     t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
     t.index ["user_id"], name: "index_shouts_on_user_id"
   end
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_121944) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
-    t.text "username"
+    t.text "username", null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
     t.index ["username"], name: "index_users_on_username", unique: true
