a57c22627ae1cc6728bcdbb2f63cf774437e49c8
Liubomyr <lyubomur.user.mv@gmail.com>
2020-03-28 10:58:59 +0200

    Add user and shout page

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -2,4 +2,8 @@ class Shout < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
+
+  delegate :username, to: :user
+
+  default_scope { order(created_at: :desc) }
 end
