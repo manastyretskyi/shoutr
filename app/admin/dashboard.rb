ef4b62aa3b5ef94bb931929e160336605ac8a2b6
Liubomyr <lyubomur.user.mv@gmail.com>
2020-03-29 23:58:14 +0300

    Add users and shouts to admin panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,31 +2,25 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Shouts" do
+          table_for Shout.all.order(created_at: :desc).limit(20).map do
+            column("Shouts") { |shout| render shout }
+            column("Actions") do |shout| 
+              link_to("View", admin_shout_path(shout)) + " " +
+              link_to("Edit", edit_admin_shout_path(shout)) + " " +
+              link_to("Delete", admin_shout_path(shout), method: :delete)
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,7 +1,9 @@
 class Shout < ApplicationRecord
   belongs_to :user
-  belongs_to :content, polymorphic: true
+  belongs_to :content, polymorphic: true, dependent: :destroy
   validates :user, presence: true
+
+  has_many :likes, foreign_key: "shout_id", dependent: :destroy
 
   delegate :username, to: :user
 end
